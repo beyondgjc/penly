0acaf4d372520c2dd1fe426b67ac8605afe7640b
beyondguo <beyondguo123@gmail.com>
2026-09-20 16:15:31 +0800

    chore(release): v5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.beyondguo.penly"
         minSdk = 29
         targetSdk = 34
-        versionCode = 4
-        versionName = "4.0.0"
+        versionCode = 5
+        versionName = "5.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
